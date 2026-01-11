20e594f6cc5ec3006162142ac8821e0821f635b6
vanshtarpe01 <vanshvtarpe@gmail.com>
2026-01-12 00:51:24 +0530

    Project No 1 Done Todo List

--- Practice/Java Scripts/Projects Of JS/To-do-list/script.js
+++ Practice/Java Scripts/Projects Of JS/To-do-list/script.js
@@ -2,11 +2,13 @@ const input = document.querySelector(".input-list");
 const button = document.querySelector(".button-list");
 const list = document.querySelector(".list-group");
 
+const  divs = document.querySelectorAll(".search");
+const btnSearch = document.querySelector(".search-btn");
+
+const searchInp = document.querySelector("#search-input");
+
 button.addEventListener("click", addTodo);
-
-
-
-
+list.addEventListener("click", deleteTodo)
 function addTodo(event){
     event.preventDefault();
 
@@ -33,3 +35,36 @@ function addTodo(event){
 
     list.appendChild(listDiv);
 }
+
+function deleteTodo(e){
+    if(e.target.classList.contains("fa-trash")){
+        // e.target.parentElement.parentElement.remove();
+        e.target.closest(".lists").remove();
+    }
+     if(e.target.classList.contains("fa-circle-check")){
+        e.target.closest(".lists").classList.toggle("check");
+    }
+}
+
+btnSearch.addEventListener("click", (e)=>{
+    e.preventDefault();
+
+    divs[0].classList.toggle("expand");
+});
+
+
+const filter = (word)=>{
+    Array.from(list.children).forEach(lists => {
+        if(lists.textContent.includes(word)){
+            lists.classList.remove("remove");
+        }else{
+            lists.classList.add("remove");
+        }
+    });
+    console.log();
+}
+
+searchInp.addEventListener("keyup", ()=>{
+    const word = searchInp.value.trim();
+    filter(word);
+});
