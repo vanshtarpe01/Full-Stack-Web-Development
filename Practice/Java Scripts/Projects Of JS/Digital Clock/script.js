f9f1baf7f4dce41f8c8e87eaa0f531f2f3547b77
vanshtarpe01 <vanshvtarpe@gmail.com>
2026-01-14 23:56:06 +0530

    Project no 4 Basic Digital Clock

---
+++ Practice/Java Scripts/Projects Of JS/Digital Clock/script.js
@@ -0,0 +1,19 @@
+const clock = document.querySelector('.clock');
+
+const time = () => {
+const now = new Date();
+
+const h = now.getHours();
+const m = now.getMinutes();
+const s = now.getSeconds();
+
+const html = `
+<span>${h}</span> :
+<span>${m}</span> :
+<span>${s}</span>
+`
+clock.innerHTML = html;
+
+}
+
+setInterval(time, 1000)
