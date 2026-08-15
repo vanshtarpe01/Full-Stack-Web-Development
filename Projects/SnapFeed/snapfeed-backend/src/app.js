d313d2d8778996d41ac3cbd008239d9952c90c7f
vanshtarpe01 <vanshvtarpe@gmail.com>
2026-08-15 21:49:13 +0530

    AI resume analyzer project initalized and basic project setting done

--- Projects/SnapFeed/snapfeed-backend/src/app.js
+++ Projects/SnapFeed/snapfeed-backend/src/app.js
@@ -1,6 +1,8 @@
 import express, { json } from "express";
+import cookieParser from 'cookie-parser';
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 export default app;
